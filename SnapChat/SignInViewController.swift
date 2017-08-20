64cd4dd452cd8cff8f2ba41f9395fa1ffc34e612
Deborah Graham <debjgraham@gmail.com>
2017-08-19 21:06:46 -0400

    added Firebase and got through creating a user (although I get email badly formatted)

---
+++ SnapChat/SignInViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SignInViewController.swift
+//  SnapChat
+//
+//  Created by Deborah Graham on 8/19/17.
+//  Copyright © 2017 Deborah Graham. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func turnUpTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: passwordTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("We tried to sign in")
+            if error != nil {
+                print("Hey we have a login error: \(error!)")
+                
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create a user")
+                    if error != nil {
+                        print("Hey we have a creation error: \(error!)")
+                    } else {
+                        print("Created User Successfully")
+                        self.performSegue(withIdentifier: "signInSegue", sender: nil)
+                    }
+                })
+            } else {
+                print("Signed in successfully")
+                self.performSegue(withIdentifier: "signInSegue", sender: nil)
+            }
+        }
+        
+        
+    }
+
+}
+
---
+++ SnapChat/SnapsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SnapsViewController.swift
+//  SnapChat
+//
+//  Created by Deborah Graham on 8/19/17.
+//  Copyright © 2017 Deborah Graham. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logoutTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
